4f36ccb21cac08bed28780b6c152c51ecff06699
emad <razavioo@gmail.com>
2024-03-06 22:56:40 +0330

    Extract transparent touch interceptor modifier

--- picker/src/main/java/dev/emad/geographic/picker/MapView.kt
+++ picker/src/main/java/dev/emad/geographic/picker/MapView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
@@ -15,9 +14,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerInputFilter
+import androidx.compose.ui.input.pointer.PointerInputModifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 
@@ -64,23 +67,8 @@ fun GeographicEntityImage(
             contentDescription = geographicEntity.name,
             modifier = Modifier
                 .offset(x = geographicEntity.x.dp, y = geographicEntity.y.dp)
-                .pointerInput(Unit) {
-                    awaitEachGesture {
-                        val event = awaitPointerEvent()
-                        val touchPosition = event.changes.first().position
-
-                        val x = (touchPosition.x / size.width * bmp.width).toInt()
-                        val y = (touchPosition.y / size.height * bmp.height).toInt()
-
-                        if (x in 0 until bmp.width && y in 0 until bmp.height) {
-                            val pixel = bmp.getPixel(x, y)
-                            val alpha = pixel ushr 24 and 0xff
-                            if (alpha != 0) {
-                                event.changes.forEach { it.consume() }
-                                onSelect(geographicEntity)
-                            }
-                        }
-                    }
+                .transparentTouchIntercept(bitmap = bmp) {
+                    onSelect(geographicEntity)
                 }
         )
     }
@@ -113,6 +101,37 @@ fun getBitmapFromVectorDrawable(context: Context, drawableId: Int): Bitmap? {
     }
     return null
 }
+
+fun Modifier.transparentTouchIntercept(bitmap: Bitmap, onTouch: () -> Unit): Modifier = then(
+    object : PointerInputModifier {
+        override val pointerInputFilter = object : PointerInputFilter() {
+            override fun onPointerEvent(
+                pointerEvent: PointerEvent,
+                pass: PointerEventPass,
+                bounds: IntSize,
+            ) {
+                if (pass == PointerEventPass.Initial) {
+                    pointerEvent.changes.forEach { change ->
+                        val position = change.position
+                        val x = (position.x / bounds.width * bitmap.width).toInt()
+                        val y = (position.y / bounds.height * bitmap.height).toInt()
+
+                        if (x in 0 until bitmap.width && y in 0 until bitmap.height) {
+                            val pixel = bitmap.getPixel(x, y)
+                            val alpha = pixel ushr 24 and 0xff
+                            if (alpha == 0) {
+                                change.consume()
+                                onTouch()
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancel() {}
+        }
+    }
+)
 
 @Preview(showBackground = true)
 @Composable
